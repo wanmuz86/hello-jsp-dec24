6a3d4ba47f9b7f8d28b8f934a0c01d6d97d6b06d
Wan Muzaffar <wanmuz86@gmail.com>
2024-12-04 16:42:01 +0800

    remove previous dependencies

--- src/main/java/com/example/spring_boot_jsp_lab_2024/controller/UserController.java
+++ src/main/java/com/example/spring_boot_jsp_lab_2024/controller/UserController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.spring_boot_jsp_lab_2024.model.User;
+import com.example.spring_boot_jsp_lab_2024.model.UserList;
 
 import jakarta.validation.Valid;
 
 @Controller
 public class UserController {
+	
+	// THis is an instance of List created just now
+	// To store the user added in the "db"
+	private UserList userList = new UserList();
 	
 	// Method GET
 	// Url in browser = /showForm
@@ -30,17 +35,32 @@ public class UserController {
 	// It will be validated thanks to @Valid
 	// The result of validation will be stored in bindingResult
 	@PostMapping("submitForm")
-	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
 		if (bindingResult.hasErrors()) {
 			return "userForm";
 			// Return user form and pass the error message
 		}
-		System.out.println("User name "+user.getName());
-		System.out.println("User email "+user.getEmail());
-		System.out.println("User phone "+user.getPhone());
-		return "formSuccess";
+//		System.out.println("User name "+user.getName());
+//		System.out.println("User email "+user.getEmail());
+//		System.out.println("User phone "+user.getPhone());
+//		return "formSuccess";
+		
+		// Add the new User to the List
+		userList.addUser(user);
+		
+		// Pass userList to the view (addAttribute)
+		model.addAttribute("userList",userList);
+		
+		// Redirect user to the userList page
+		return "userList";
 				
 		}
+	
+	@GetMapping("/userList")
+	public String showUserList(Model model) {
+		model.addAttribute("userList",userList);
+		return "userList";
+	}
 	
 	
 	
