1ae8094dd6145881021fe55018411b5f8e92e502
Wan Muzaffar <wanmuz86@gmail.com>
2024-12-04 11:52:24 +0800

    first commit

---
+++ src/main/java/com/example/spring_boot_jsp_lab_2024/SpringBootJspLab2024Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jsp_lab_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspLab2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspLab2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_jsp_lab_2024/SpringBootJspLab2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jsp_lab_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspLab2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
